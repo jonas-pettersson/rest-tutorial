1596c67aebe735054706f812c81f334302b22df8
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-09-24 16:59:42 +0200

    snippet has user

--- snippets/views.py
+++ snippets/views.py
@@ -10,6 +10,10 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        return super().perform_create(serializer)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
